7c11bfa935cebdc452474dd63e5b4081d7e2a3d9
Sebas-RR <25sebasr@gmail.com>
2025-08-24 23:18:57 -0500

    Inicialización del microservicio de solicitudes con scaffold

---
+++ applications/app-service/src/main/java/com/pragma/crediya/solicitudes/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package com.pragma.crediya.solicitudes.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "com.pragma.crediya.solicitudes.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
